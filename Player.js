eeaf47589f58bf62d57486f9f4d23bec49fbbd47
Zach-Williams <zachw@aie.edu.au>
2016-08-17 12:24:58 +1000

    Edited player to use a vec2
    Added the vec2 to the index file.
    Changed the this.x and this.y to a position vector

--- Player.js
+++ Player.js
@@ -1,8 +1,10 @@
 var Player = function () 
 {
     this.image = document.createElement("img");
-    this.x = canvas.width / 2;
-    this.y = canvas.height / 2;
+    //this.x = canvas.width / 2;
+    //this.y = canvas.height / 2;
+    this.position = new Vector2();
+    this.position.set(canvas.width / 2,canvas.height / 2);
     this.width = 159;
     this.height = 163;
     this.image.src = "hero.png";
@@ -23,7 +25,7 @@ Player.prototype.update = function (deltaTime) {
 
 Player.prototype.draw = function () {
     context.save();
-    context.translate(this.x, this.y);
+    context.translate(this.position.x, this.position.y);
     context.rotate(this.rotation);
     context.drawImage(this.image, -this.width / 2, -this.height / 2);
     context.restore();
